e81d4348b2384da0e1dad4417710d2546fc90a4a
Nick Fontana <nick@Nicks-MacBook.local>
2020-05-20 15:44:01 -0500

    output symbol input to terminal

--- ui.py
+++ ui.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QMainWindow, QInputDialog
+from PyQt5.QtWidgets import QMainWindow, QInputDialog, QLineEdit
 from ui_main import Ui_MainWindow
 from simpleBot import *
 
-class MainWindow(QWidget):
+class MainWindow:
 
     def __init__(self, parent = None):
         self.main_win = QMainWindow()
@@ -13,16 +13,18 @@ class MainWindow(QWidget):
 
         self.ui.MakeMoneyButton.clicked.connect(self.on_MAKEMONEY_click)
 
+
     def show(self):
         self.main_win.show()
 
     def on_MAKEMONEY_click(self):
 
         # test:
-        text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Stock symbol:')
+        #text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Stock symbol:')
+        symbol_output = self.ui.stockSymbol.text()
 
         self.ui.OutputText.setText("POGGERS")
-        print(text)
+        print(symbol_output)
         # Check for non empty inputs for symbol and margins
         ####################
 
